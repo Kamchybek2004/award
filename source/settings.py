6ef37f2d2b89c1da1647430f87df92d4632f619a
Kamchy <kamchy2501@gmail.com>
2025-10-07 10:51:15 +0600

    Настройка settings.py ещё раз

--- source/settings.py
+++ source/settings.py
@@ -97,7 +97,7 @@ USE_TZ = True
 
 # Статика
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # PK по умолчанию
